6ff5fa41b070ca4348372e13ac2fce89cd5b9cc7
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-07-18 20:49:12 +0530

    Package published

---
+++ index.js
@@ -0,0 +1,12 @@
+function generaterandompassword(length) {
+    var result = '';
+    var characters = '12345678!@#$%^&*()_+=-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+module.exports = generaterandompassword;
